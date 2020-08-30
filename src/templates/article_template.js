1e1bc7498a727a33b5e2b6d8ecfb9bfc66c6b562
DarkroomLaboratory <darkroomassociation@gmail.com>
2020-08-30 21:01:58 +0400

    wrapped article templates in common layout

--- src/templates/article_template.js
+++ src/templates/article_template.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from "gatsby"
 import { graphql } from 'gatsby'
+import Layout from "../components/layout"
 export default function Template({data}){
     const article  = data.markdownRemark
 
     return (
-        <div className="wrapperArticle">
-            <Link to="/">Go Back</Link>
-            <hr/>
-            <h1>{ article.frontmatter.title }</h1>
-            <h4>Posted by { article.frontmatter.author } | { article.frontmatter.date }</h4>
-            <div dangerouslySetInnerHTML={{__html: article.html}}/>
-        </div>
+        <Layout>
+            <div className="wrapperArticle">
+                {/* <Link to="/">Go Back</Link> */}
+                <hr/>
+                <h1>{ article.frontmatter.title }</h1>
+                <h4>Posted by { article.frontmatter.author } | { article.frontmatter.date }</h4>
+                <div dangerouslySetInnerHTML={{__html: article.html}}/>
+            </div>
+        </Layout>
     )
 }
 
